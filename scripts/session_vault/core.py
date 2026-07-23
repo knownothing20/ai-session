9e5a675bce0986999a4c1a361126df93c1d6dbf7
knownothing20 <137740359+knownothing20@users.noreply.github.com>
2026-07-23 10:34:06 +0800

    Export native restore API

--- scripts/session_vault/core.py
+++ scripts/session_vault/core.py
@@ -1,5 +1,7 @@
 """Compatibility exports for the public synchronization API."""
+
 from .operations import describe_layout, inspect_adapter, sync_archive, verify_archive
+from .restore import restore_archive
 from .utils import SyncError, derive_machine_id, vault_machine_root
 
 __all__ = [
@@ -7,6 +9,7 @@ __all__ = [
     "derive_machine_id",
     "describe_layout",
     "inspect_adapter",
+    "restore_archive",
     "sync_archive",
     "vault_machine_root",
     "verify_archive",
